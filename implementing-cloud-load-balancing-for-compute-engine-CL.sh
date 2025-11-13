96fe1ac6779e56a3c934027e9a32cac10319e935
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-11-13 13:54:32 -0600

    cleaned up

--- implementing-cloud-load-balancing-for-compute-engine-CL.sh
+++ implementing-cloud-load-balancing-for-compute-engine-CL.sh
@@ -1,16 +1,14 @@
-
 #!/bin/bash
 
-# Fetch zone and region
 ZONE=$(gcloud compute project-info describe \
   --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 REGION=$(gcloud compute project-info describe \
   --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 PROJECT_ID=$(gcloud config get-value project)
 
-
-
-
+# ------------------
+# ----- TASK 1 -----
+# ------------------
 gcloud compute instances create web1 \
 --zone=$ZONE \
 --machine-type=e2-small \
@@ -47,30 +45,21 @@ apt-get install apache2 -y
 service apache2 restart
 echo "<h3>Web Server: web3</h3>" | tee /var/www/html/index.html'
 
-
-
-
-
+# ------------------
+# ----- TASK 2 -----
+# ------------------
 gcloud compute firewall-rules create www-firewall-network-lb --allow tcp:80 --target-tags network-lb-tag
-
-
-
 
 gcloud compute addresses create network-lb-ip-1 \
     --region=$REGION  
 
-
-
 gcloud compute http-health-checks create basic-check
 
-
- gcloud compute target-pools create www-pool \
-    --region=$REGION  --http-health-check basic-check
-
+gcloud compute target-pools create www-pool \
+  --region=$REGION  --http-health-check basic-check
 
 gcloud compute target-pools add-instances www-pool \
     --instances web1,web2,web3 --zone=$ZONE
-    
 
 gcloud compute forwarding-rules create www-rule \
     --region=$REGION \
@@ -78,13 +67,11 @@ gcloud compute forwarding-rules create www-rule \
     --address network-lb-ip-1 \
     --target-pool www-pool
 
-
 IPADDRESS=$(gcloud compute forwarding-rules describe www-rule --region=$REGION  --format="json" | jq -r .IPAddress)
 
-
-
-#TASK 3
-
+# ------------------
+# ----- TASK 3 -----
+# ------------------
 gcloud compute instance-templates create lb-backend-template \
    --region=$REGION \
    --network=default \
@@ -104,13 +91,8 @@ gcloud compute instance-templates create lb-backend-template \
      tee /var/www/html/index.html
      systemctl restart apache2'
 
-
-
-
 gcloud compute instance-groups managed create lb-backend-group \
    --template=lb-backend-template --size=2 --zone=$ZONE 
-
-
 
 gcloud compute firewall-rules create fw-allow-health-check \
   --network=default \
@@ -120,25 +102,16 @@ gcloud compute firewall-rules create fw-allow-health-check \
   --target-tags=allow-health-check \
   --rules=tcp:80
 
-
-
 gcloud compute addresses create lb-ipv4-1 \
   --ip-version=IPV4 \
   --global
-
-
 
 gcloud compute addresses describe lb-ipv4-1 \
   --format="get(address)" \
   --global
 
-
-
-
 gcloud compute health-checks create http http-basic-check \
   --port 80
-
-
 
 gcloud compute backend-services create web-backend-service \
   --protocol=HTTP \
@@ -146,24 +119,16 @@ gcloud compute backend-services create web-backend-service \
   --health-checks=http-basic-check \
   --global
 
-
-
 gcloud compute backend-services add-backend web-backend-service \
   --instance-group=lb-backend-group \
   --instance-group-zone=$ZONE \
   --global
 
-
-
 gcloud compute url-maps create web-map-http \
     --default-service web-backend-service
 
-
-
-
 gcloud compute target-http-proxies create http-lb-proxy \
     --url-map web-map-http
-
 
 gcloud compute forwarding-rules create http-content-rule \
     --address=lb-ipv4-1\
